ef53b9cedd8a145e7b4f5394810824b980d69d49
juneyng <120372886+juneyng@users.noreply.github.com>
2023-06-17 08:43:39 +0000

    SpringBasic 5 Commit

---
+++ basicspring-hello/src/main/java/basicspring/basicspringhello/BasicspringHelloApplication.java
@@ -0,0 +1,12 @@
+package basicspring.basicspringhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicspringHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicspringHelloApplication.class, args);
+	}
+}
---
+++ basicspring-hello/src/test/java/basicspring/basicspringhello/BasicspringHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package basicspring.basicspringhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicspringHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
